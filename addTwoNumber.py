c492685c94460d471a6eff3551794b64cd5f1775
nacho <91359628+nachodall@users.noreply.github.com>
2023-08-08 15:56:31 -0300

    Update addTwoNumber.py

--- addTwoNumber.py
+++ addTwoNumber.py
@@ -8,33 +8,25 @@ class Solution:
         def getNumber(node1: Optional[ListNode]) -> int:
             listOfNumbers = []
             currentNode = node1
-
             while currentNode != None:
                 listOfNumbers.append(currentNode.val)
                 currentNode = currentNode.next
-
             number = 0
-
             for i in range(len(listOfNumbers)-1,-1,-1):
                 digit = listOfNumbers[i]
                 number = number * 10 + digit
-            
             return number
         
         def createLinkedList(resultado: int) -> [ListNode]:
             listOfNumbers = []
             linkedList = ListNode(resultado % 10)
-
             while resultado > 0:
                 listOfNumbers.append(resultado % 10)
                 resultado = resultado // 10
-
             current = linkedList
-
             for value in listOfNumbers[1:]:
                 current.next = ListNode(value)
                 current = current.next
-
             return linkedList
 
         resultado = getNumber(l1) + getNumber(l2)
